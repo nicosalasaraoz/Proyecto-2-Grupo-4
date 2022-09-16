95df0984f2fc030b1ae7e7caa932425c214d562c
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-09-15 23:26:30 -0300

    modificacion contacto

--- index.js/contacto.js
+++ index.js/contacto.js
@@ -1,10 +1,13 @@
-function validar() {
+function validarForm() {
     let textoUno = document.getElementById('texto1').value
-    let textoDos = document.getElementById('texto2').value    
-    if(textoUno === '' || textoDos === ''){
+    let textoDos = document.getElementById('texto2').value   
+    let textotres = document.getElementById('texto3').value
+    if(textoUno === '' || textoDos === '' || textotres === '' ){
         document.getElementById('texto1').className = 'form-control is-invalid'
         document.getElementById('texto2').className = 'form-control is-invalid'
+        document.getElementById ('texto3').className= 'form-control is-invalid'
     }else{
         document.getElementById('texto1').className = 'form-control is-valid'
         document.getElementById('texto2').className = 'form-control is-valid'
+        document.getElementById('texto3').className = 'form-control is-valid'
     }}
