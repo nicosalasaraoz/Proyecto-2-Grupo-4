1efd273dd4599478fea74b60ada6cedc37e4b299
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2022-09-21 18:56:54 -0300

    cambios en login register html css y js. tabla user y juegos

---
+++ index.js/register.js
@@ -0,0 +1,34 @@
+function validarForm() {
+    let user = document.getElementById('user').value;
+    let pass = document.getElementById('pass').value;
+    let validPass = document.getElementById('validPass').value
+    let localSTG = JSON.parse(localStorage.getItem('usuario')) || []
+
+    const array = []
+
+    if (user === '' || pass === '' || validPass === '') {
+        alert('formulario en blanco')
+    } else {
+
+        for (let i = 0; i < localSTG.length; i++) {
+            const usuarioEnLocalSTG = localSTG[i];
+            array.push(usuarioEnLocalSTG)
+        }
+
+        let userExists = localSTG.filter(local => local.user === user)
+            console.log(userExists)
+        if (userExists.length === 0) {
+            if (pass === validPass) {
+                array.push({ id: localSTG.length + 1, user, pass, role: 'user' })
+                localStorage.setItem('usuario', JSON.stringify(array))
+                console.log(array)
+            } else {
+                alert('contraseñas no son iguales')
+            }
+
+        }else{
+            alert('usuario ya existe')
+        }
+
+    }
+}
