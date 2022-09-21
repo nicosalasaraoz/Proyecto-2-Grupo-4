b9420f3ede696063e694c9de373848d39d7cfec3
Dani Gavier <92883295+DaniGavier@users.noreply.github.com>
2022-09-21 15:04:14 -0300

    fix of files

--- index.js/users.js
+++ index.js/users.js
@@ -1,38 +1,39 @@
-let productsFiltered = PRODUCTS;
 const busqueda = document.getElementById('busqueda');
-const renderGames = () => 
-{
+const renderGames = () => {
   games.innerHTML = '';
+  const q = document.getElementById("busqueda").value.toLowerCase();
 
-  productsFiltered.forEach(product => 
-  {
-    const card = document.createElement('div');
-    card.className = 'col';
-    card.innerHTML = 
-    `<div class="card h-100">
-    <img src="${product.img}" alt="">
-			<div class="card-body">
-        <h5 class="card-title">${product.name}</h5>
-        <p class="card-text">${product.descripcion}</p>
-      </div>
-      <div class="card-footer border-0">
-      </div>
-      <div class="bg-white d-flex">
-        <a href="games.html" id= ${product.id}" class="btn btn-danger ms-auto m-2"> Ver mas
-        </a>
-      </div>
-		 </div>`;
-    games.appendChild(card);
+  PRODUCTS.forEach(category => {
+    const title = document.createElement('div');
+    title.className = 'col-12';
+    title.innerHTML = `<p class="display-6 text-light mt-3"> ${category.category} </p>`;
+    games.appendChild(title);
+
+    category.products.forEach (product => {
+      if (product.name.toLocaleLowerCase().includes(q) || category.category.toLocaleLowerCase().includes(q) ){
+        const card = document.createElement('div');
+        card.className = 'col-12 col-sm-6 col-md-4 col-lg-4 col-xl-2 my-4 justify-content-center';
+
+        card.innerHTML = `
+          <div class="card h-100">
+            <img src="${product.img}" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.descripcion}</p>
+            </div>
+            <div class="card-footer border-0">
+              <div class="bg-white d-flex">
+                <a href="games.html" id= ${product.id}" class="btn btn-danger ms-auto m-2"> Ver mas
+                </a>
+              </div>
+            </div>
+          </div>`;
+        games.appendChild(card);
+      }
+      
+    });
+
   });
 };
 
-const searchFilter = () => {
-  productsFiltered = PRODUCTS.filter(p =>
-    p.name
-      .toUpperCase()
-      .includes(busqueda.value.toUpperCase())
-  );
-  renderGames();
-};
-renderGames()
-
+renderGames();
