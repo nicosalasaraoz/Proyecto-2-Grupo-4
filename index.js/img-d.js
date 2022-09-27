e27154bf2fa380256685aeb1afad605a9898a918
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2022-09-27 20:36:43 -0300

    agregando imagen destacada js

--- index.js/img-d.js
+++ index.js/img-d.js
@@ -1,16 +1,9 @@
-let productos = JSON.parse(localStorage.getItem('productos'))
-let divRow = document.getElementById('rowCard')
-divRow.innerHTML = productos.map(producto =>
-    ` 
-    <div class="card" style="width: 18rem;">
-        <img src="${producto.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"${producto.marca}</h5>
-                <p class="card-text">${producto.descripcion}</p>
-                <p class="card-text">${producto.precio}</p>
-                <button type="button" class="btn btn-success my-3">Ver Mas...</button>
-                <button type="button" class='btn btn-dark'>Añadir al Carrito</button>
-
-            </div>
-    </div>`
-)
+let products = JSON.parse(localStorage.getItem('products') || '[]');
+let imgdestacada = document.getElementById('img-des')
+imgdestacada.innerHTML = productos.map(producto =>
+    `<div class="card-body">
+        <h5 class="card-title">${producto.name}</h5>
+        <p class="card-text ">${producto.descripcion}</p>
+          <a href="games.html?productId=${producto.id}" class="btn mt-2 btn-secondary">Ver mas</a>
+      </div>
+      <img src="${producto.img}" class="img-top m-0 px-3 pb-3" alt="...">`)
