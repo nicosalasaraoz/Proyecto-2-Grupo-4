93c349fb8ac077fb07413a30ec5b8602fcb0f16b
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2022-09-28 09:08:51 -0300

    agragando el for de los productos al local storage

--- index.js/img-d.js
+++ index.js/img-d.js
@@ -1,6 +1,31 @@
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('productId');
+const imgd = () => {
+let products = JSON.parse(localStorage.getItem('products') || '[]');
+    if (products.length === 0) {
+        for(category of PRODUCTS) {
+        const cat = categories.find(el => el.name === category.category);
+
+        for(product of category.products) {
+            let productId = products.length + 1;
+            products.push({
+            id: productId,
+            categoryId: cat.id, 
+            name: product.name,
+            img: product.img, 
+            descripcion: product.descripcion,
+            video: product.video,
+            });
+        }
+        }
+        localStorage.setItem("products", JSON.stringify(products));  
+    }
+}
+imgd();
+
 let products = JSON.parse(localStorage.getItem('products') || '[]');
 let imgdestacada = document.getElementById('img-des')
-imgdestacada.innerHTML = productos.map(producto =>
+imgdestacada.innerHTML = products.map(producto =>
     `<div class="card-body">
         <h5 class="card-title">${producto.name}</h5>
         <p class="card-text ">${producto.descripcion}</p>
