78257627539b241a4298bfe6582e869c65a89e7f
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2022-09-27 22:02:50 -0300

    agregando script

--- index.js/img-d.js
+++ index.js/img-d.js
@@ -4,6 +4,6 @@ imgdestacada.innerHTML = productos.map(producto =>
     `<div class="card-body">
         <h5 class="card-title">${producto.name}</h5>
         <p class="card-text ">${producto.descripcion}</p>
-          <a href="games.html?productId=${producto.id}" class="btn mt-2 btn-secondary">Ver mas</a>
-      </div>
-      <img src="${producto.img}" class="img-top m-0 px-3 pb-3" alt="...">`)
+        <a href="games.html?productId=${producto.id}" class="btn mt-2 btn-secondary">Ver mas</a>
+    </div>
+    <img src="${producto.img}" class="img-top m-0 px-3 pb-3" alt="...">`)
